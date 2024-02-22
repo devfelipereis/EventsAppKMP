7a1a106255321eb454508c5f8010a2fc6c942d4e
Felipe Reis <feelipereis7@gmail.com>
2024-02-21 22:33:18 -0300

    Loading events in a list view

--- composeApp/src/commonMain/kotlin/presentation/screens/explore/ExploreScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/explore/ExploreScreen.kt
@@ -1,15 +1,27 @@
 package presentation.screens.explore
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import domain.model.Event
+import presentation.components.EventCard
 
 data object ExploreScreen : Screen {
     @Composable
@@ -26,11 +38,33 @@ data object ExploreScreen : Screen {
             targetState = eventsState
         ) { state ->
             when (state) {
-                is ExploreScreenState.Loading -> Text("Loading")
+                is ExploreScreenState.Loading -> ProgressIndicator()
                 is ExploreScreenState.Error -> Text("Error")
                 is ExploreScreenState.Empty -> Text("Empty")
-                is ExploreScreenState.Loaded -> Text("Events: ${state.data.size}")
+                is ExploreScreenState.Loaded -> ListView(state.data)
             }
         }
+    }
+}
+
+@Composable
+fun ListView(events: List<Event>) {
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(events) { event ->
+            EventCard(event)
+        }
+    }
+}
+
+@Composable
+fun ProgressIndicator() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator()
     }
 }
